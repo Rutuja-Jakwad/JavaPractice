c84c7ad6c6c2a36354db564defb22d1969065b1c
Rutuja Jakwad <rutujajakwad585@gmail.com>
2021-09-22 09:22:20 +0530

    Refactored: contents of the message

--- src/main/java/com/javapractice/App.java
+++ src/main/java/com/javapractice/App.java
@@ -12,7 +12,7 @@ public class App
     	private static final Logger LOG = LogManager.getLogger(App.class);
     	public static void main( String[] args )
     	{
-    		String message = "Hello, World";
+    		String message = "Hello I am learning the Java language";
     		LOG.debug(message + " Will Be Printed on Debug");
     		LOG.info(message + " Will Be Printed On Info");
     		LOG.warn(message + " Will Be Printed On Warn");
