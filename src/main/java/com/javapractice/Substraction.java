ec09a9907e141d6e3c02f378cab00d6d7d5b11bf
Rutuja Jakwad <rutujajakwad585@gmail.com>
2021-09-23 21:57:55 +0530

    Added: Substraction

---
+++ src/main/java/com/javapractice/Substraction.java
@@ -0,0 +1,12 @@
+package com.javapractice;
+
+public class Substraction {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int Sub;
+		Sub = a - b;
+		System.out.println("Substraction of these numbers:" +Sub);
+	}
+
+}
